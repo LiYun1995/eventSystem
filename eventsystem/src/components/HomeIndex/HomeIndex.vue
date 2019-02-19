d0a96e5654bf477125827fbbb990c59452b18992
LiYun <616852971@qq.com>
2019-02-19 16:20:51 +0800

    create

---
+++ eventsystem/src/components/HomeIndex/HomeIndex.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+    <Row>
+        <Col span="24"><HomeNav></HomeNav></Col>
+    </Row>
+    <Row>
+        <Col span="6">col-8</Col>
+        <Col span="12">col-8</Col>
+        <Col span="6">col-8</Col>
+    </Row>
+     <Row>
+        <Col span="6">col-8</Col>
+        <Col span="12">col-8</Col>
+        <Col span="6">col-8</Col>
+    </Row>
+     <Row>
+        <Col span="6">col-8</Col>
+        <Col span="12">col-8</Col>
+        <Col span="6">col-8</Col>
+    </Row>
+     <Row>
+        <Col span="24">col-8</Col>
+    </Row>
+    </div>
+</template>
+
+<script>
+    import HomeNav from '@/components/HomeIndex/nav'
+    export default{
+        name:'HomeIndex',
+        components:{HomeNav}
+    }
+</script>
+
---
+++ eventsystem/src/components/HomeIndex/nav.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Card>
+      <Row>
+        <Col span="8">{{ time }}</Col>
+        <Col span="8">
+          <h1>可视化值班系统</h1>
+        </Col>
+        <Col span="8">
+          <p>硬盘：共3T，剩余50G</p>
+          <p>温度：22摄氏度</p>
+        </Col>
+      </Row>
+    </Card>
+  </div>
+</template>
+
+<script>
+import { setInterval } from "timers";
+export default {
+  data() {
+    return {
+      time: new Date().toString()
+    };
+  },
+  created() {
+    setInterval(() => {
+      this.time = new Date().toString();
+    }, 1000);
+  },
+  methods: {}
+};
+</script>
+
